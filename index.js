2482c3502505ffb63adf56f4a9fa2482ed341033
arunlajayan <arunlajayan@gmail.com>
2023-12-11 14:53:21 +0530

    socket connection

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ io.on('connection', (socket, req) => {
     socket.emit('welcome','welcome to the websocket server');
     socket.on('message', data => {
         console.log(data);
-    })
+    }) 
 }) 
 server.listen(3000)
